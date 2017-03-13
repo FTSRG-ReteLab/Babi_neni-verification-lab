c069188fb4a3cdf87e36da495007e537e80f912d
englert.bruno <englert.bruno@>
2017-03-13 16:28:29 +0100

    Unit testing

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,34 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.system.TrainSystem;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainController controller;
+    TrainSensor sensor;
+    TrainUser user;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        TrainSystem system = new TrainSystem();
+        controller = system.getController();
+        sensor = system.getSensor();
+        user = system.getUser();
+
+        sensor.overrideSpeedLimit(50);
     }
 
     @Test
-    public void thisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void _1() {
+
+        Assert.assertEquals(10, controller.getReferenceSpeed());
     }
+
+
 }
