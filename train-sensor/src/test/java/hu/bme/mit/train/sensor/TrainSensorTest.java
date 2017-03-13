5790bd034460dcda04de9e43744993a197197344
jazzisnice <burjan.viktor@gmail.com>
2017-03-13 08:50:31 -0700

    Added mockito

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,23 +3,24 @@ package hu.bme.mit.train.sensor;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.system.TrainSystem;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
 
 public class TrainSensorTest {
 
-    TrainController controller;
+    //TrainController controller;
     TrainSensor sensor;
     TrainUser user;
 
+    TrainController controller = mock(TrainController.class);
+
     @Before
     public void before() {
-        TrainSystem system = new TrainSystem();
-        controller = system.getController();
-        sensor = system.getSensor();
-        user = system.getUser();
+
 
         sensor.overrideSpeedLimit(50);
     }
